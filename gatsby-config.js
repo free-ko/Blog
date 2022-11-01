a0900a0f2aafdd5cb5da968c0795af6d5b783ca3
Kay <duddnr654@naver.com>
2022-11-01 23:09:55 +0900

    [CHORE] gatsby-plugin-google-gtag 설치

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,6 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
  */
 module.exports = {
   siteMetadata: {
-    // You can overwrite values here that are used for the SEO component
-    // You can also add new values here to query them like usual
-    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
     siteTitle: `고영욱의 블로그`,
     siteTitleAlt: `👋 고영욱의 블로그`,
     siteHeadline: `👋 고영욱의 블로그`,
